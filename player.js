d2a40426f22fc1d7328dde7a12fe96ecc192b77a
Ethan Choi <ethanchoi.812@gmail.com>
2019-11-20 16:06:17 +0800

    Update 'getMove' and 'validMove' functions

--- player.js
+++ player.js
@@ -9,28 +9,38 @@ const Player = (type) => {
      * @param board from Gameboard
      *
      */
-    const getMove = (board) => {
+    const getMove = (board, arr=[]) => {
         let move;
 
         if (type === 'bot'){
-
             // random move
-            move = Math.floor(Math.random() * Math.floor(board.width));
-            validMove(move);
+            let row = Math.floor(Math.random() * Math.floor(board.width));
+            let col = Math.floor(Math.random() * Math.floor(board.width));
+
+            move = [row, col]
+            if (validMove(move)) {
+                board.receiveAttack(move);
+            }
 
         } else {
-
             // get move from user
-            move = '';
-            validMove(move);
+            move = arr;
+            if (validMove(move)) {
+                board.receiveAttack(move);
+            }
         }
-
     }
 
     const validMove = (move) => {
-        if (!attempts.includes(move)) {
+
+        const matchMove = (attempt) => {
+            attempt[0] === move[0] && attempt[1] === move[1];
+        }
+
+        // returns true if not found in 'attempts' array
+        if (attempts.findIndex(matchMove) === -1) {
             attempts.push(move);
-            return move;
+            return true;
         } else {
             return false;
         }
